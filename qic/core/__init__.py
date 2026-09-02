f486f5e2aa43a59a81aaa5685b830ce3d997d6a2
Ivan Pasev <ivan.p.pasev@gmail.com>
2026-09-02 07:59:05 +0300

    QIC-G1: export canonicalization and digest API

--- qic/core/__init__.py
+++ qic/core/__init__.py
@@ -1,6 +1,25 @@
 """Trusted constitutional core for QIC."""
 
+from .canonical import (
+    CANONICAL_VERSION,
+    CanonicalizationError,
+    canonical_bytes,
+    canonical_text,
+)
 from .constitution import PRIME_LAWS, ConstitutionSnapshot
+from .digest import DIGEST_VERSION, DigestDomainError, digest_bytes, digest_hex
 from .maturity import Maturity
 
-__all__ = ["ConstitutionSnapshot", "Maturity", "PRIME_LAWS"]
+__all__ = [
+    "CANONICAL_VERSION",
+    "DIGEST_VERSION",
+    "CanonicalizationError",
+    "ConstitutionSnapshot",
+    "DigestDomainError",
+    "Maturity",
+    "PRIME_LAWS",
+    "canonical_bytes",
+    "canonical_text",
+    "digest_bytes",
+    "digest_hex",
+]
